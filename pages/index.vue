47e0ef7aa0263be98656dd65a427e3d42574a750
arthursb2016 <artsborba@gmail.com>
2023-02-07 09:45:03 -0300

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+console.log('hello')
+</script>
+
+<template>
+  <h1>Days Tracker</h1>
+  <h2>Count days since a past date or track how many days left until an upcoming event!</h2>
+</template>
