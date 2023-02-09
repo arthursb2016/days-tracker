cdc771c407e52b78e50e570bde9085b70f60bfbf
arthursb2016 <artsborba@gmail.com>
2023-02-09 16:56:23 -0300

    viewport meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css',
@@ -12,4 +13,13 @@ export default defineNuxtConfig({
       'process.env.DEBUG': false,
     }
   },
+  app: {
+    head: {
+      title: 'Days Tracker',
+      viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
+      meta: [
+        { name: 'description', content: 'Date difference calculator, with tracking days options.' }
+      ],
+    }
+  }
 })
